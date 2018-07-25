8f5cd2ffa753807d46b372bca958dd477479b124
Arpit <rpittrer@gmail.com>
2018-07-25 12:57:25 +0530

    runninggit

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,9 @@
-var Driver = artifacts.require("./Driver.sol");
+var Driver = artifacts.require('./Driver.sol');
+var expo = artifacts.require('./Shipper_Exporter.sol');
+var impo = artifacts.require('./Shipper_Importer.sol');
 
-module.exports = function (deployer) {
+module.exports = function(deployer) {
     deployer.deploy(Driver);
-}
+    deployer.deploy(expo);
+    deployer.deploy(impo);
+};
